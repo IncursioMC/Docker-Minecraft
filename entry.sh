033cca1284d01091530ff3c868cf09b1a9286d1d
SupremeMortal <SupremeMortal@users.noreply.github.com>
2017-06-30 10:36:01 +0100

    Remove checks for existing jar

--- entry.sh
+++ entry.sh
@@ -15,30 +15,22 @@ fi
 # Determine what is being considered as the start file.
 if [ "${SERVER_JARFILE}" == "waterfall.jar" ]; then
     SERVER_JARFILE="waterfall.jar"
-    if [ -f "/home/container/${SERVER_JARFILE}" ]; then
-        echo "${C_GREEN}Found ${SERVER_JARFILE} in container, not downloading a new jar.${C_RESET}"
-    else 
         echo ":/home/container$ curl -sS https://ci.incursio.net/job/Travertine/${DL_VERSION}/artifact/Travertine-Proxy/bootstrap/target/Travertine.jar -o ${SERVER_JARFILE}"      
         curl -sS https://ci.incursio.net/job/Travertine/${DL_VERSION}/artifact/Travertine-Proxy/bootstrap/target/Travertine.jar -o ${SERVER_JARFILE}
         if [ $? -ne 0 ]; then
             echo "${C_RED}PTDL_CONTAINER_ERR: There was an error while attempting to download a new jarfile for this server.${C_RESET}"
             exit 1
         fi
-    fi
     
 elif [ -z "${SERVER_JARFILE}" ] || [ "${SERVER_JARFILE}" == "bungeecord.jar" ]; then
     SERVER_JARFILE="bungeecord.jar"
 # Download the correct version, or skip if it already exists.
-    if [ -f "/home/container/${SERVER_JARFILE}" ]; then
-        echo "${C_GREEN}Found ${SERVER_JARFILE} in container, not downloading a new jar.${C_RESET}"
-    else
         echo ":/home/container$ curl -sS https://ci.incursio.net/job/Bungeecord-Alpine/${DL_VERSION}/artifact/bootstrap/target/BungeeCord.jar -o ${SERVER_JARFILE}"
         curl -sS https://ci.incursio.net/job/Bungeecord-Alpine/${DL_VERSION}/artifact/bootstrap/target/BungeeCord.jar -o ${SERVER_JARFILE}
         if [ $? -ne 0 ]; then
             echo "${C_RED}PTDL_CONTAINER_ERR: There was an error while attempting to download a new jarfile for this server.${C_RESET}"
             exit 1
         fi
-    fi
 fi
 
 # Output Current Java Version
